62aaa211aa294f460e5c70c6165d49341784b046
lijun <likumb@outlook.com>
2024-09-01 17:33:50 +0800

    更新导航栏和英文首页

--- src/.vuepress/navbar/en.ts
+++ src/.vuepress/navbar/en.ts
@@ -1,31 +1,9 @@
 import { navbar } from "vuepress-theme-hope";
 
 export const enNavbar = navbar([
-  "/",
-  "/portfolio",
-  "/demo/",
+  "/guide/",
   {
-    text: "Guide",
-    icon: "lightbulb",
-    prefix: "/guide/",
-    children: [
-      {
-        text: "Bar",
-        icon: "lightbulb",
-        prefix: "bar/",
-        children: ["baz", { text: "...", icon: "ellipsis", link: "#" }],
-      },
-      {
-        text: "Foo",
-        icon: "lightbulb",
-        prefix: "foo/",
-        children: ["ray", { text: "...", icon: "ellipsis", link: "#" }],
-      },
-    ],
-  },
-  {
-    text: "V2 Docs",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/",
+    text: "Download",
+    link: "https://apps.apple.com/cn/app/id1614115295",
   },
 ]);
--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -6,9 +6,4 @@ export const zhNavbar = navbar([
     text: "下载",
     link: "https://apps.apple.com/cn/app/id1614115295",
   },
-  {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
 ]);
